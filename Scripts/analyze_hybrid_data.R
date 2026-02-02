adae9a6e6daf8d431e1ee0b0302d0812f4daf1a7
Emily Ryznar <emily.ryznar@nmfs.local>
2026-02-02 14:22:35 -0800

    analyze hybrid

--- Scripts/analyze_hybrid_data.R
+++ Scripts/analyze_hybrid_data.R
@@ -114,3 +114,128 @@ write.csv(rbind(female, male), "./Data/hybrid_bioabund.csv")
 ggplot(hybrid_bioabund, aes(YEAR, ABUNDANCE, color = CATEGORY))+
   geom_line(linewidth = 1) +
   theme_bw()
+
+# Analyze size distribution
+hybrid_spec <- readRDS("./Data/hybrid_specimen.rda")$specimen
+
+pp <- hybrid_spec %>% filter(YEAR == 2025)
+
+ggplot()+
+  geom_histogram(pp %>% filter(SEX != 4), mapping = aes(SIZE)) +
+  facet_wrap(~SEX, nrow = 2, scales = "free")+
+  theme_bw()
+
+# Fit models
+
+
+diagnose <- function(model){
+  model.name <- deparse(substitute(model))
+  acf(resid(model))
+  ss <- summary(model) # model summary
+  
+  gam.check(model) # make sure smooth terms are appropriate
+  
+  plot(simulateResiduals(model)) # Checks uniformity, dispersion, outliers via DHARMa
+  
+  # plot facetted smooths
+  sm.dat <- smooth_estimates(model) %>%
+    pivot_longer(., cols = 6:ncol(.), names_to = "resp", values_to = "value")
+  
+  ggplot(sm.dat, aes(x = value, y = .estimate)) +
+    geom_ribbon(sm.dat, mapping = aes(ymin = .estimate + 2 * .se, ymax = .estimate - 2 * .se), fill = "cadetblue", alpha = 0.25)+
+    geom_line(color = "cadetblue", linewidth = 1.25) +
+    facet_wrap(~ .smooth, scales = "free_x", ncol = 2) +   # facet by smooth term name
+    theme_bw()+
+    ggtitle(model.name)+
+    ylab("Partial effect")+
+    xlab("Value")+
+    theme(axis.text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          strip.text = element_text(size = 14)) -> sm.plot
+  
+  return(list(ss, sm.plot))
+}
+
+hybrid_abund <- read.csv("./Data/hybrid_bioabund.csv") %>%
+  filter(CATEGORY %in% c("mature_female", "large_male")) %>%
+  filter(YEAR > 1989) %>%
+  group_by(YEAR) %>%
+  reframe(ABUNDANCE = sum(ABUNDANCE))
+
+
+snow_abund <- read.csv("./Data/snow_bioabund.csv") %>%
+  filter(YEAR > 1989) %>%
+  group_by(YEAR) %>%
+  reframe(ABUNDANCE = sum(ABUNDANCE))
+
+tanner_abund <- read.csv("./Data/tanner_bioabund.csv") %>%
+  filter(YEAR > 1989) %>%
+  group_by(YEAR) %>%
+  reframe(ABUNDANCE = sum(ABUNDANCE))
+
+
+overlap <- rbind(read.csv("./Data/bhatt_malesnow.femTanner.csv"),
+                 read.csv("./Data/bhatt_maleTanner.femsnow.csv")) %>%
+  filter(YEAR > 1989) %>%
+  na.omit()
+
+ice <- read.csv("./Data/ice_means_1989-2025.csv") %>%
+  group_by(year) %>%
+  reframe(value = mean(value)) %>%
+  filter(year> 1989 & year != 2020)
+
+lag = 3
+mod.dat <- data.frame(year = unique(hybrid_abund$YEAR), 
+                      ice = lag(ice$value,6),
+                      hybrid_abund = lag(hybrid_abund$ABUNDANCE/1e6),
+                      snow_abund = lag(snow_abund$ABUNDANCE/1e6,3),
+                      tanner_abund = lag(tanner_abund$ABUNDANCE/1e6, 3),
+                      # hybrid_matfem = (hybrid_abund$ABUNDANCE[hybrid_abund$CATEGORY == "mature_female"])/1e6,
+                      # hybrid_lgmale = (hybrid_abund$ABUNDANCE[hybrid_abund$CATEGORY == "large_male"])/1e6,
+                      # snow_matfem = (snow_abund$ABUNDANCE[snow_abund$CATEGORY == "mature_female"])/1e6,
+                      # snow_lgmale = (snow_abund$ABUNDANCE[snow_abund$CATEGORY == "large_male"])/1e6,
+                      # tanner_matfem = (tanner_abund$ABUNDANCE[tanner_abund$CATEGORY == "mature_female"])/1e6,
+                      # tanner_lgmale = (tanner_abund$ABUNDANCE[tanner_abund$CATEGORY == "large_male"])/1e6,
+                      bhatt_snowmale_tannerfem = lag(overlap$bhatt[overlap$comparison == "snowlgmale-tannermatfem"], 0),
+                      bhatt_tannermale_snowfem = lag(overlap$bhatt[overlap$comparison == "tannerlgmale-snowmatfem"]), 0)
+
+library(corrplot)
+
+# Select numeric columns and compute correlation
+cor_vars <- mod.dat %>%
+  dplyr::select(ice, snow_abund, tanner_abund, 
+                bhatt_snowmale_tannerfem, bhatt_tannermale_snowfem) %>%
+  na.omit()  # drop rows with any NA
+
+cor_mat <- cor(cor_vars, use = "complete.obs")
+
+# Plot
+corrplot(cor_mat, 
+         method = "circle",
+         type = "upper",
+         order = "hclust",
+         tl.col = "black",
+         tl.srt = 45,
+         addCoef.col = "black",
+         number.cex = 0.7)
+
+
+
+hybrid.gam <- gam(
+  hybrid_abund ~  s(bhatt_snowmale_tannerfem, k=4) +
+    s(bhatt_tannermale_snowfem, k = 4) + 
+    s(ice, k=4) + 
+    s(tanner_abund, k = 4)+
+    s(snow_abund, k = 4),
+  data = mod.dat,
+  family = nb()
+)
+
+acf(residuals(hybrid.gam))
+
+gam.check(hybrid.gam)
+summary(hybrid.gam)
+
+res_dharma <- simulateResiduals(hybrid.gam, n = 1000)
+plot(res_dharma)
+
